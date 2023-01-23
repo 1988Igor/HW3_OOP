2bce4b16ff1d5965a2fb52153c1910957e30fb74
1988Igor <110403993+1988Igor@users.noreply.github.com>
2023-01-23 22:53:52 +0100

    corrected in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,15 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.LinkedList;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyLinkedList<Integer>myList = new MyLinkedList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+        for (Integer item:myList) System.out.println(item);
+
     }
 }
